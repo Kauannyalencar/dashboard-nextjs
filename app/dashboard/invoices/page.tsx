1b21420602d31c9b208836047d2307f4015706a0
Kauanny Alencar <kauannyalencar19@gmail.com>
2024-06-17 12:46:08 -0300

    first commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+import {poppins} from '@/app/ui/fonts'
+ 
+ export default function Page() {
+    return <p className={`${poppins.className}`}>Invoices Page</p>;
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter } from "next/font/google";
+export const inter = Inter({ subsets: ['latin']})
+
+import { Lusitana } from "next/font/google";
+export const lusitana = Lusitana({weight: ['400', '700'], subsets: ['latin']})
+
+import { Poppins } from "next/font/google";
+
+export const poppins = Poppins({weight: ['400', '900'], subsets: ['latin']})
